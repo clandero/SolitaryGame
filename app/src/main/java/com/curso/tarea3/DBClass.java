2120bd22bcd06cde2bb51a6ea5e987964e2b042b
clandero <clandero@udec.cl>
2019-06-25 14:51:18 -0400

    Testing de base de datos

--- app/src/main/java/com/curso/tarea3/DBClass.java
+++ app/src/main/java/com/curso/tarea3/DBClass.java
@@ -10,39 +10,37 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBClass {
-    // Nombre de la base de datos, y tabla asociada
     private static final String DATABASE_NAME = "MiDB";
     private static final String DATABASE_TABLE = "solitario";
     private static final int DATABASE_VERSION = 1;
-    // Las constantes que representan las columnas de la tabla
+
     private static final String FILAID = "_id";
     private static final String NOMBRE = "nombre";
     private static final String VICTORIAS = "victorias";
     private static final String DERROTAS = "derrotas";
+    private static final String TAG = "DBClass";
 
-    private static final String TAG = "DBHelper";
-
-    // Este String contiene el comando SQL para la creación de la base de datos
-    private static final String DATABASE_CREATE = "create table " + DATABASE_TABLE + "(" + FILAID + " integer primary key, " + NOMBRE + " text not null," + VICTORIAS + "integer not null," + DERROTAS + " integer not null);";
+    private static final String DATABASE_CREATE = "create table " + DATABASE_TABLE + "(" + FILAID + " integer primary key autoincrement, " + NOMBRE + " text not null," + VICTORIAS + " integer not null," + DERROTAS + " integer not null);";
     private final Context contexto; // Contexto de la aplicacion
     private DatabaseHelper Helper; // Clase interna para acceso a base de datos SQL
     private SQLiteDatabase db; // La base de datos SQL
-    public DBClass(Context contexto) {
+
+    DBClass(Context contexto) {
         //Log.i("alo","olaola");
         this.contexto = contexto;
         Helper = new DatabaseHelper(contexto);
     }
     // Clase privada interna para acceso a base de datos SQL
     private static class DatabaseHelper extends SQLiteOpenHelper {
-        private Context context;
         DatabaseHelper(Context contexto) {
             super(contexto, DATABASE_NAME, null, DATABASE_VERSION);
-            context = contexto;
         }
         @Override
         public void onCreate(SQLiteDatabase db) {
             try {
+                Log.w(TAG,"Creando la base de datos");
                 db.execSQL(DATABASE_CREATE);
+                //db.execSQL("create table solitario(_id integer primary key autoincrement,nombre text not null,victorias integer not null,derrotas integer not null)");
             }
             catch(SQLException e) {
                 e.printStackTrace();
@@ -53,6 +51,7 @@ public class DBClass {
             Log.w(TAG, "Actualizando la base de datos desde la versión " + versionAnterior +
                     " a la versión " + versionNueva);
             db.execSQL("DROP TABLE IF EXISTS contactos");
+            onCreate(db);
         }
     }
     // Abre la base de datos para escritura
@@ -66,21 +65,21 @@ public class DBClass {
         Helper.close();
     }
 
-    public long agregaJugador(String nombre,int victorias, int derrotas){
+    public long agregaJugador(String nombre, int victorias, int derrotas){
         ContentValues valores = new ContentValues();
         valores.put(NOMBRE, nombre);
         valores.put(VICTORIAS, victorias);
         valores.put(DERROTAS, derrotas);
         return db.insert(DATABASE_TABLE, null, valores);
     }
-    public boolean eliminaJugador(long filaID){
+    public boolean eliminaJugadorPorId(long filaID){
         return db.delete(DATABASE_TABLE,FILAID + "="+filaID,null)>0;
     }
     public Cursor recuperaJugadores(){
         return db.query(DATABASE_TABLE,new String[]{FILAID,NOMBRE,VICTORIAS,DERROTAS},null,null,null,null,null);
     }
 
-    public Cursor recuperaJugador(long filaID) throws SQLException {
+    public Cursor recuperaJugadorPorId(long filaID) throws SQLException {
         Cursor c = db.query(DATABASE_TABLE, new String[]{FILAID,NOMBRE,VICTORIAS,DERROTAS},FILAID+"="+filaID,null,null,null,null);
         if (c != null){
             c.moveToFirst();
