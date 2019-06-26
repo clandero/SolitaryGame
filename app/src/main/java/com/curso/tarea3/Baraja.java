f9c489221f5127d1774b983b32ace420fa248c24
vanessarriagada <vanessarriagada@udec.cl>
2019-06-26 12:55:40 -0400

    Se crearon las clases Baraja y Cartas.

---
+++ app/src/main/java/com/curso/tarea3/Baraja.java
@@ -0,0 +1,113 @@
+package com.curso.tarea3;
+
+import android.util.Log;
+import android.util.Pair;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Vector;
+
+
+public class Baraja {
+    private Vector<Carta> mazo_pica;
+    private Vector<Carta> mazo_trebol;
+    private Vector<Carta> mazo_diamante;
+    private Vector<Carta> mazo_corazon;
+    private Vector<Deque<Carta>> mazo_restante;
+    private int numero_carta[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    private int[] numero_carta_restante;
+    private Carta a_1, a_2, a_3, a_4;
+    private String palo[] = { "pica", "trebol", "diamante", "corazon"};
+
+
+    public void inicio(){
+        //inicio mazos superiores
+        mazo_pica = new Vector<Carta>();
+        mazo_trebol = new Vector<Carta>();
+        mazo_diamante = new Vector<Carta>();;
+        mazo_corazon = new Vector<Carta>();
+        a_1 = new Carta();
+        a_1.setNumero(1);
+        a_1.setPalo("pica");
+        a_1.setEstado("visible");
+        mazo_pica.add(a_1);
+        a_2 = new Carta();
+        a_2.setNumero(1);
+        a_2.setPalo("trebol");
+        a_2.setEstado("visible");
+        mazo_trebol.add(a_2);
+        a_3 = new Carta();
+        a_3.setNumero(1);
+        a_3.setPalo("diamante");
+        a_3.setEstado("visible");
+        mazo_diamante.add(a_3);
+        a_4 = new Carta();
+        a_4.setNumero(1);
+        a_4.setPalo("corazon");
+        a_4.setEstado("visible");
+        mazo_corazon.add(a_4);
+        Log.d("ESTADO ",mazo_pica.get(0).getPalo());
+        //inicio 12 mazos
+        knuth_pilas();
+        Vector<Carta> temporal = new Vector<Carta>();
+        for (int j = 0 ; j < 4 ; j++) {
+            for (int i = 0; i < 12; i++) {
+                Carta carta_temporal = new Carta();
+                carta_temporal.setNumero(numero_carta[i]);
+                carta_temporal.setPalo(palo[j]);
+                carta_temporal.setEstado("invisible");
+                temporal.add(carta_temporal);
+            }
+        }
+        mazo_restante = new Vector<Deque<Carta>>();
+        inicializando_numero_carta_restante();
+        knuth_mazo_restante();
+        int j = 0;
+        for (int i = 0; i < 48; i++){
+            if (i == 0){
+                Deque<Carta> mazo_temporal = new LinkedList<Carta>();
+                mazo_restante.add(mazo_temporal);
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+            }
+            else if(i != 0 && (i % 4) == 0){
+                Deque<Carta> mazo_temporal = new LinkedList<Carta>();
+                mazo_restante.add(mazo_temporal);
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+                j++;
+            }
+            else {
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+            }
+        }
+        for (int i = 0; i < mazo_restante.size(); i ++){
+                Log.d("AQUIII ",String.valueOf(mazo_restante.elementAt(i).getFirst().getNumero()));
+                Log.d("AQUIII ",mazo_restante.elementAt(i).getFirst().getPalo());
+        }
+    }
+    public void inicializando_numero_carta_restante(){
+        numero_carta_restante = new int[48];
+        for(int i = 0; i < 48; i++){
+            numero_carta_restante[i] = i;
+        }
+    }
+
+    public void knuth_pilas(){
+        Random r = new Random();
+        for (int i = 11; i > 0; i--) {
+            int indice = r.nextInt(i);
+            int tmp = numero_carta[indice];
+            numero_carta[indice] = numero_carta[i];
+            numero_carta[i] = tmp;
+        }
+    }
+    public void knuth_mazo_restante(){
+        Random r = new Random();
+        for (int i = 47; i > 0; i--) {
+            int indice = r.nextInt(i);
+            int tmp = numero_carta_restante[indice];
+            numero_carta_restante[indice] = numero_carta_restante[i];
+            numero_carta_restante[i] = tmp;
+        }
+    }
+}
--- app/src/main/java/com/curso/tarea3/MainActivity.java
+++ app/src/main/java/com/curso/tarea3/MainActivity.java
@@ -1,13 +1,129 @@
 package com.curso.tarea3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+public class MainActivity extends Activity {
+    DBClass db;
+    Baraja nueva;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DBClass(this);
+        nueva = new Baraja();
+        nueva.inicio();
+
+    }
+    public void agregaJugadores(View view) {
+// Agrega los contactos iniciales
+        db.abre();
+        db.agregaJugador("Bill Gates", 0,0);
+        db.agregaJugador("Sheldon Cooper", 0,0);
+        db.agregaJugador("Homer Simpson", 0,0);
+        db.agregaJugador("James Bond", 0,0);
+        db.agregaJugador("Jon Snow", 0,0);
+        db.cierra();
+    }
+    public void buscaJugadorPorId(View view) {
+        EditText et = findViewById(R.id.IDBuscar);
+        long id = Integer.parseInt(et.getText().toString());
+        DisplayToast("Se busca el contacto (" + id + ")");
+        db.abre();
+        Cursor c = db.recuperaJugadorPorId(id);
+        if (c.moveToFirst()) {
+            MuestraJugador(c);
+        }
+        else {
+            DisplayToast("Jugador no fue encontrado");
+        }
+        c.close();
+        db.cierra();
+    }
+    public void buscaJugadorPorNombre(View view) {
+//Recupera un contacto por nombre
+        EditText et = findViewById(R.id.NombreBuscar);
+        String nombre = et.getText().toString();
+        DisplayToast("Se busca el jugador (" + nombre + ")");
+        db.abre();
+        Cursor c = db.recuperaJugadorPorNombre(nombre);
+        if (c.moveToFirst()) {
+            MuestraJugador(c);
+        }
+        else {
+            DisplayToast("Jugador no fue encontrado");
+        }
+        while(c.moveToNext()) {
+            MuestraJugador(c);
+        }
+        c.close();
+        db.cierra();
+    }
+    public void actualizaJugador(View view) {
+// Actualiza un contacto
+        EditText etID = findViewById(R.id.IDAgregar);
+        long id = Integer.parseInt(etID.getText().toString());
+        EditText etNombre = findViewById(R.id.NombreAgregar);
+        String nombre = etNombre.getText().toString();
+        EditText etVictorias = findViewById(R.id.VictoriasAgregar);
+        String victorias = etVictorias.getText().toString();
+        EditText etDerrotas = findViewById(R.id.DerrotasAgregar);
+        String derrotas = etDerrotas.getText().toString();
+        DisplayToast("Se actualiza el contacto " + id + " con el nombre "
+                + nombre + " victorias: " + victorias + " y derrotas: " + derrotas);
+        db.abre();
+        if (db.actualizaJugador(id, nombre, Integer.parseInt(victorias), Integer.parseInt(derrotas))) {
+            DisplayToast("Actualización exitosa");
+        }
+        else {
+            DisplayToast("Actualización fallida");
+        }
+        db.cierra();
+    }
+    public void eliminaJugadorPorId(View view) {
+// Elimina un contacto
+        EditText et = findViewById(R.id.IDEliminar);
+        long id = Integer.parseInt(et.getText().toString());
+        DisplayToast("Se elimina el contacto (" + id + ")");
+        db.abre();
+        if (db.eliminaJugadorPorId(id)) {
+            DisplayToast("Eliminación exitosa");
+        }
+        else {
+            DisplayToast("Eliminación fallida");
+        }
+        db.cierra();
+    }
+    public void eliminaJugadores(View view) {
+        DisplayToast("Elimina todos los Jugadores");
+        db.eliminaJugadores();
+        DisplayToast("Base de datos Contactos está vacía");
+    }
+    public void muestraJugadores(View view) {
+// Recupera contactos
+        db.abre();
+        Cursor c = db.recuperaJugadores();
+        if (c.moveToFirst()) {
+            do {
+                MuestraJugador(c);
+            } while(c.moveToNext());
+        }
+        else {
+            DisplayToast("Base de datos Contactos está vacía");
+        }
+        c.close();
+        db.cierra();
+    }
+    public void MuestraJugador(Cursor c) {
+        DisplayToast("id: " + c.getString(0) + "\n" +
+                "nombre: " + c.getString(1) + "\n" +
+                "victorias: " + c.getString(2) + "\n" +
+                "derrotas: " + c.getString(3) + "\n");
+    }
+    private void DisplayToast(String mensaje) {
+        Toast.makeText(getBaseContext(), mensaje, Toast.LENGTH_SHORT).show();
     }
 }
