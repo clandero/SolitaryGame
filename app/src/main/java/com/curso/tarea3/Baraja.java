d3d260efabfbbbdc57a209fd57b13ad5c58d782a
Sergio Saavedra <sesaavedra@udec.cl>
2019-07-02 15:35:02 -0400

    reemplazo de drag por click. Adición de timer y nuevas cartas. Falta mapeo de cartas.

---
+++ app/src/main/java/com/curso/tarea3/Baraja.java
@@ -0,0 +1,238 @@
+package com.curso.tarea3;
+
+import android.util.Log;
+import android.util.Pair;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Vector;
+
+
+public class Baraja {
+    private Vector<Carta> base_pica;
+    private Vector<Carta> base_trebol;
+    private Vector<Carta> base_diamante;
+    private Vector<Carta> base_corazon;
+    private Vector<Deque<Carta>> mazo_restante;
+    private int numero_carta[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    private int[] numero_carta_restante;
+    private Carta a_1, a_2, a_3, a_4;
+    private String palo[] = { "pica", "trebol", "diamante", "corazon"};
+
+
+    public void inicio(){
+        //inicio mazos superiores
+        base_pica = new Vector<Carta>();
+        base_trebol = new Vector<Carta>();
+        base_diamante = new Vector<Carta>();;
+        base_corazon = new Vector<Carta>();
+        a_1 = new Carta();
+        a_1.setNumero(1);
+        a_1.setPalo("pica");
+        a_1.setEstado("visible");
+        base_pica.add(a_1);
+        a_2 = new Carta();
+        a_2.setNumero(1);
+        a_2.setPalo("trebol");
+        a_2.setEstado("visible");
+        base_trebol.add(a_2);
+        a_3 = new Carta();
+        a_3.setNumero(1);
+        a_3.setPalo("diamante");
+        a_3.setEstado("visible");
+        base_diamante.add(a_3);
+        a_4 = new Carta();
+        a_4.setNumero(1);
+        a_4.setPalo("corazon");
+        a_4.setEstado("visible");
+        base_corazon.add(a_4);
+        Log.d("ESTADO ",base_pica.get(0).getPalo());
+        //inicio 12 mazos
+        knuth_pilas();
+        Vector<Carta> temporal = new Vector<Carta>();
+        for (int j = 0 ; j < 4 ; j++) {
+            for (int i = 0; i < 12; i++) {
+                Carta carta_temporal = new Carta();
+                carta_temporal.setNumero(numero_carta[i]);
+                carta_temporal.setPalo(palo[j]);
+                carta_temporal.setEstado("invisible");
+                temporal.add(carta_temporal);
+            }
+        }
+        //traspaso de mazo_temporal a mazo_restante
+        mazo_restante = new Vector<Deque<Carta>>();
+        inicializando_numero_carta_restante();
+        knuth_mazo_restante();
+        int j = 0;
+        for (int i = 0; i < 48; i++){
+            if (i == 0){
+                Deque<Carta> mazo_temporal = new LinkedList<Carta>();
+                mazo_restante.add(mazo_temporal);
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+                Log.d("NUEVO ",String.valueOf(mazo_restante.elementAt(j).getLast().getNumero()));
+                Log.d("NUEVO ",mazo_restante.elementAt(j).getLast().getPalo());
+            }
+            else if(i != 0 && (i % 4) == 0){
+                Deque<Carta> mazo_temporal = new LinkedList<Carta>();
+                mazo_restante.add(mazo_temporal);
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+                Log.d("NUEVO ",String.valueOf(mazo_restante.elementAt(j).getLast().getNumero()));
+                Log.d("NUEVO ",mazo_restante.elementAt(j).getLast().getPalo());
+                j++;
+            }
+            else {
+                mazo_restante.elementAt(j).add(temporal.elementAt(numero_carta_restante[i]));
+                Log.d("NUEVO ",String.valueOf(mazo_restante.elementAt(j).getLast().getNumero()));
+                Log.d("NUEVO ",mazo_restante.elementAt(j).getLast().getPalo());
+            }
+        }
+
+        for (int i = 0; i < mazo_restante.size(); i ++){
+            mazo_restante.elementAt(i).getFirst().setEstado("visible");
+        }
+    }
+    //guarda las posiciones posibles del mazo_temporal
+    public void inicializando_numero_carta_restante(){
+        numero_carta_restante = new int[48];
+        for(int i = 0; i < 48; i++){
+            numero_carta_restante[i] = i;
+        }
+    }
+    //desordenar de números en cada pilas
+    public void knuth_pilas(){
+        Random r = new Random();
+        for (int i = 11; i > 0; i--) {
+            int indice = r.nextInt(i);
+            int tmp = numero_carta[indice];
+            numero_carta[indice] = numero_carta[i];
+            numero_carta[i] = tmp;
+        }
+    }
+    //desordenar todas las cartas del mazo_restante
+    public void knuth_mazo_restante(){
+        Random r = new Random();
+        for (int i = 47; i > 0; i--) {
+            int indice = r.nextInt(i);
+            int tmp = numero_carta_restante[indice];
+            numero_carta_restante[indice] = numero_carta_restante[i];
+            numero_carta_restante[i] = tmp;
+        }
+    }
+    //insertar carta de una de las 12 pilas a una base
+    public void insertar_a_base(int index_pila_inicio, String palo_base_destino){
+        if(mazo_restante.elementAt(index_pila_inicio).size() != 0){
+            if(mazo_restante.elementAt(index_pila_inicio).getFirst().getPalo() == palo_base_destino){
+                if(palo_base_destino == "pica"){
+                    if ((mazo_restante.elementAt(index_pila_inicio).getFirst().getNumero() - 1) == base_pica.lastElement().getNumero()){
+                        base_pica.lastElement().setEstado("invisible");
+                        base_pica.add(mazo_restante.elementAt(index_pila_inicio).getFirst());
+                        mazo_restante.elementAt(index_pila_inicio).removeFirst();
+                        if(mazo_restante.elementAt(index_pila_inicio).size() != 0){
+                            mazo_restante.elementAt(index_pila_inicio).getFirst().setEstado("visible");
+                        }
+                    }
+                }
+                else if (palo_base_destino == "trebol"){
+                    if ((mazo_restante.elementAt(index_pila_inicio).getFirst().getNumero() - 1) == base_trebol.lastElement().getNumero()){
+                        base_trebol.lastElement().setEstado("invisible");
+                        base_trebol.add(mazo_restante.elementAt(index_pila_inicio).getFirst());
+                        mazo_restante.elementAt(index_pila_inicio).removeFirst();
+                        if(mazo_restante.elementAt(index_pila_inicio).size() != 0){
+                            mazo_restante.elementAt(index_pila_inicio).getFirst().setEstado("visible");
+                        }
+                    }
+                }
+                else if (palo_base_destino == "diamante"){
+                    if ((mazo_restante.elementAt(index_pila_inicio).getFirst().getNumero() - 1) == base_diamante.lastElement().getNumero()){
+                        base_diamante.lastElement().setEstado("invisible");
+                        base_diamante.add(mazo_restante.elementAt(index_pila_inicio).getFirst());
+                        mazo_restante.elementAt(index_pila_inicio).removeFirst();
+                        if(mazo_restante.elementAt(index_pila_inicio).size() != 0){
+                            mazo_restante.elementAt(index_pila_inicio).getFirst().setEstado("visible");
+                        }
+                    }
+                }
+                else if (palo_base_destino == "corazon"){
+                    if ((mazo_restante.elementAt(index_pila_inicio).getFirst().getNumero() - 1) == base_corazon.lastElement().getNumero()){
+                        base_corazon.lastElement().setEstado("invisible");
+                        base_corazon.add(mazo_restante.elementAt(index_pila_inicio).getFirst());
+                        mazo_restante.elementAt(index_pila_inicio).removeFirst();
+                        if(mazo_restante.elementAt(index_pila_inicio).size() != 0){
+                            mazo_restante.elementAt(index_pila_inicio).getFirst().setEstado("visible");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //inserción entre cartas de las 12 pilas.
+    public void insertar_a_pila(int index_pila_inicio, int index_pila_destino){
+        if((mazo_restante.elementAt(index_pila_inicio).size() > 0) && (mazo_restante.elementAt(index_pila_destino).size() > 0) ){
+            if (mazo_restante.elementAt(index_pila_inicio).getFirst().getPalo() == mazo_restante.elementAt(index_pila_destino).getFirst().getPalo()){
+                if ((mazo_restante.elementAt(index_pila_inicio).getFirst().getNumero() - 1) == mazo_restante.elementAt(index_pila_destino).getFirst().getNumero()){
+                    mazo_restante.elementAt(index_pila_destino).getFirst().setEstado("invisible");
+                    mazo_restante.elementAt(index_pila_destino).addFirst(mazo_restante.elementAt(index_pila_inicio).getFirst());
+                    mazo_restante.elementAt(index_pila_inicio).removeFirst();
+                    if (mazo_restante.elementAt(index_pila_inicio).size() != 0){
+                        mazo_restante.elementAt(index_pila_inicio).getFirst().setEstado("visible");
+                    }
+                }
+            }
+        }
+    }
+    public void repartir(){
+        Vector<Carta> temporal = new Vector<Carta>();
+        for (int i = 0; i < mazo_restante.size(); i++){
+            int k = mazo_restante.elementAt(i).size();
+            for (int j = 0; j < k; j++){
+                mazo_restante.elementAt(i).getFirst().setEstado("invisible");
+                temporal.add(mazo_restante.elementAt(i).getFirst());
+                mazo_restante.elementAt(i).removeFirst();
+            }
+        }
+        int k = 0;
+        for (int i = 0; i < mazo_restante.size(); i++){
+            for (int j = 0; j < 4; j++){
+                if(temporal.size() == k){
+                    break;
+                }
+                else {
+                    mazo_restante.elementAt(i).addLast(temporal.elementAt(k));
+                    Log.d("REPARTIR_NUMERO ", String.valueOf(mazo_restante.elementAt(i).getLast().getNumero()));
+                    Log.d("REPARTIR_PALO ", mazo_restante.elementAt(i).getLast().getPalo());
+                    if (j == 0){
+                        mazo_restante.elementAt(i).getFirst().setEstado("visible");
+                    }
+                    k++;
+                }
+            }
+            if (temporal.size() == k){
+                break;
+            }
+        }
+
+    }
+    //chequear si el juego se terminó
+    public boolean juego_terminado(){
+        if ((base_pica.size() == 13) && (base_trebol.size() == 13) && (base_diamante.size() == 13) && (base_corazon.size() == 13)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public int cartas_en_bases(){
+        return (base_pica.size() + base_trebol.size() + base_diamante.size() + base_corazon.size() - 4);
+    }
+
+    public void insertar(){//INCOMPLETO
+        for(int i = 0; i < mazo_restante.size(); i++){
+            for(int j = 0; j < mazo_restante.elementAt(i).size(); j++){
+                if(mazo_restante.elementAt(j).getLast().getPalo() == "corazon"){
+
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/curso/tarea3/GameActivity.java
+++ app/src/main/java/com/curso/tarea3/GameActivity.java
@@ -4,6 +4,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,17 +23,46 @@ import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
+    //variables para guardar estado
+    private static final int move = 0;
+    private static final long time_play = 0;
+    private static final int card = 0;
+    private static final Baraja saved_baraja = null;
+    private Handler mHandler = new Handler();
+
+    //variables del juego
+    private Baraja baraja;
     private int movements;
+    private long mStartTime = 0L;
+    private int cartas_correctas;
+    private long time = mStartTime;
+    private long aux_time = 0;
     private TextView movimientos,cartas,tiempo;
     private ImageView shuffle,pica,corazon,trebol,diamante;
     private ImageView[][] cards;
     private final int CARD_ID[][] = {{R.id.card_1,R.id.card_2,R.id.card_3,R.id.card_4},
             {R.id.card_5,R.id.card_6,R.id.card_7,R.id.card_8},{R.id.card_9,R.id.card_10,R.id.card_11,R.id.card_12}};
+    private final int CARD_IMAGE[] ={R.drawable.c2,R.drawable.c3,R.drawable.c4,R.drawable.c5,R.drawable.c6,R.drawable.c7,R.drawable.c8,R.drawable.c9,
+            R.drawable.c10,R.drawable.jc,R.drawable.qc,R.drawable.kc,R.drawable.d2,R.drawable.d3,R.drawable.d4,R.drawable.d5,R.drawable.d6,R.drawable.d7,
+            R.drawable.d8,R.drawable.d9,R.drawable.d10,R.drawable.jd,R.drawable.qd,R.drawable.kd,R.drawable.p2,R.drawable.p3,R.drawable.p4,R.drawable.p5,
+            R.drawable.p6,R.drawable.p7,R.drawable.p8,R.drawable.p9,R.drawable.p10,R.drawable.jp,R.drawable.qp,R.drawable.kp,R.drawable.t1,R.drawable.t2,
+            R.drawable.t3,R.drawable.t4,R.drawable.t5,R.drawable.t6,R.drawable.t7,R.drawable.t8,R.drawable.t9,R.drawable.t10,R.drawable.jt,R.drawable.qt,
+            R.drawable.kt};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        baraja = new Baraja();
+        baraja.inicio();
+
+        if(mStartTime==0L){
+            mStartTime= SystemClock.uptimeMillis();
+            mHandler.removeCallbacks(mUpdateTimeTask);
+            mHandler.postDelayed(mUpdateTimeTask,100);
+        }
+
 
         //Area de contadores
         movimientos = findViewById(R.id.movimientos);
@@ -41,20 +72,34 @@ public class GameActivity extends AppCompatActivity {
         //Area de decks finales
         shuffle = findViewById(R.id.shuffle);
         pica = findViewById(R.id.pica_deck);
+        pica.setImageResource(R.drawable.p1);
         corazon = findViewById(R.id.corazon_deck);
+        corazon.setImageResource(R.drawable.c1);
         trebol = findViewById(R.id.trebol_deck);
+        trebol.setImageResource(R.drawable.t1);
         diamante = findViewById(R.id.diamante_deck);
-
-//        pica.setOnDragListener(new MyDragListener());
+        diamante.setImageResource(R.drawable.d1);
+        cartas_correctas = 4;
+        cartas.setText(Integer.toString(cartas_correctas));
 
         //Area de juego
         cards = new ImageView[3][4];
         for (int i = 0; i < 3;i++)
             for(int j = 0; j < 4;j++){
                 cards[i][j] =this.findViewById(CARD_ID[i][j]);
-                cards[i][j].setImageResource(R.drawable.back_card);
-                cards[i][j].setOnTouchListener(new MyTouchListener());
+                cards[i][j].setImageResource(R.drawable.d2);
+                cards[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //sumar movimiento
+                        movements++;
+                        movimientos.setText(Integer.toString(movements));
+                        //verificar movimiento posible
+
+                    }
+                });
             }
+//        F l i p
 //        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(pica, "scaleX", 1f, 0f);
 //        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(pica, "scaleX", 0f, 1f);
 //        oa1.setInterpolator(new DecelerateInterpolator());
@@ -66,7 +111,7 @@ public class GameActivity extends AppCompatActivity {
 //                pica.setImageResource(R.drawable.media_shuffle);
 //                oa2.start();
 //            }
-//        }); FLIP
+//        });
 //        oa1.start();
 
 
@@ -77,55 +122,47 @@ public class GameActivity extends AppCompatActivity {
             }
         });
     }
-    private final class MyTouchListener implements View.OnTouchListener {
 
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                ClipData data = ClipData.newPlainText("", "");
-                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-                        view);
-                view.startDrag(data, shadowBuilder, view, 0);
-                movements++;
-                movimientos.setText(String.valueOf(movements));
-                return true;
-            } else {
-                return false;
-            }
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("move",movements);
+        outState.putLong("time_play",aux_time);
+        outState.putInt("card",cartas_correctas);
+        super.onSaveInstanceState(outState);
+
     }
 
-//    class MyDragListener implements View.OnDragListener {
-//        Drawable enterShape = getResources().getDrawable(
-//                R.drawable.back_card);
-//        Drawable normalShape = getResources().getDrawable(R.drawable.back_card);
-//
-//        @Override
-//        public boolean onDrag(View v, DragEvent event) {
-//            int action = event.getAction();
-//            switch (event.getAction()) {
-//                case DragEvent.ACTION_DRAG_STARTED:
-//                    // do nothing
-//                    break;
-//                case DragEvent.ACTION_DRAG_ENTERED:
-//                    v.setBackground(enterShape);
-//                    break;
-//                case DragEvent.ACTION_DRAG_EXITED:
-//                    v.setBackground(normalShape);
-//                    break;
-//                case DragEvent.ACTION_DROP:
-//                    // Dropped, reassign View to ViewGroup
-//                    View view = (View) event.getLocalState();
-//                    ViewGroup owner = (ViewGroup) view.getParent();
-//                    owner.removeView(view);
-//                    owner.addView(view);
-//                    view.setVisibility(View.VISIBLE);
-//                    break;
-//                case DragEvent.ACTION_DRAG_ENDED:
-//                    v.setBackground(normalShape);
-//                default:
-//                    break;
-//            }
-//            return true;
-//        }
-//    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        movements = savedInstanceState.getInt("move");
+        time = savedInstanceState.getLong("time_play");
+        cartas_correctas = savedInstanceState.getInt("card");
+        movimientos.setText(Integer.toString(movements));
+        cartas.setText(Integer.toString(cartas_correctas));
+    }
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            final long start = mStartTime;
+            long millis = SystemClock.uptimeMillis() - start + time;
+            aux_time = millis;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            tiempo.setText("" + minutes + ":"
+                    + String.format("%02d", seconds));
+            mHandler.postDelayed(this, 200);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        mHandler.removeCallbacks(mUpdateTimeTask);
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mUpdateTimeTask, 100);
+    }
 }
